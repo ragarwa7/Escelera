1b3311dec7393860e2de120f6b3aecac4ca4f4b9
Rachit Agarwal <ragarwa7@ncsu.edu>
2017-09-20 22:53:28 -0400

    rentapp.rachit.1: added roles for users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create :name => 'Yash' , :email => 'ybajori@ncsu.edu', :password => 'yash123', :role => 2
+User.create :name =>'Racs', :email => 'rachit.grwl9@gmail.com', :password => 'racs123', :role => 1
